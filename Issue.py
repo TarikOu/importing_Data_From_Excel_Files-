b94e9c3f66179af7e66ac3d9d27bcc13c49fde3d
Tarik OUAREZKI <tarikliketarik@gmail.com>
2024-01-10 09:46:17 +0100

    .

--- Issue.py
+++ Issue.py
@@ -8,10 +8,9 @@ Created on Tue Jan  9 14:35:28 2024
 import pandas as pd
 
 def import_data_from_excel(file_path):
-    try:
-        data = pd.read_excel(file_path)
-        # Process the imported data
-        return data
+    data = pd.read_excel(file_path)
+    # Process the imported data
+    return data
     
 
 import_data_from_excel('records.xlsx')
